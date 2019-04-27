b54dd123ae646096f911985248d71af742a9d3d4
Brandon O <32948553+BioFan90@users.noreply.github.com>
2019-04-26 22:51:01 -0400

    copper and recipes

--- java/biofan/oresmod/init/ModBlocks.java
+++ java/biofan/oresmod/init/ModBlocks.java
@@ -24,44 +24,67 @@ public class ModBlocks
 {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
+	//Blocks
+	public static final Block BLOCK_COPPER = new BlockMetal("block_copper", Material.IRON);
+	public static final Block BLOCK_TIN = new BlockMetal("block_tin", Material.IRON);
+	public static final Block BLOCK_NICKEL = new BlockMetal("block_nickel", Material.IRON);
+	
+	public static final Block BLOCK_SILVER = new BlockMetal("block_silver", Material.IRON);
+	public static final Block BLOCK_PLATINUM = new BlockMetal("block_platinum", Material.IRON);
+	public static final Block BLOCK_STEEL = new BlockMetal("block_steel", Material.IRON);
+	public static final Block BLOCK_BRONZE = new BlockMetal("block_bronze", Material.IRON);
+	public static final Block BLOCK_VULCANITE = new BlockMetal("block_vulcanite", Material.IRON);
+	
+	public static final Block BLOCK_RUBY = new BlockMetal("block_ruby", Material.IRON);
+	public static final Block BLOCK_SAPHIRE = new BlockMetal("block_saphire", Material.IRON);
+	public static final Block BLOCK_AMETHYST = new BlockMetal("block_amethyst", Material.IRON);
+	public static final Block BLOCK_JADE = new BlockMetal("block_jade", Material.IRON);
+	public static final Block BLOCK_PHOSPHOLITE = new BlockMetal("block_phospholite", Material.IRON);
+	public static final Block BLOCK_ONYX = new BlockMetal("block_onyx", Material.IRON);
+	public static final Block BLOCK_ROSE_QUARTZ = new BlockMetal("block_rose_quartz", Material.IRON);
+	public static final Block BLOCK_GARNET = new BlockMetal("block_garnet", Material.IRON);
+	public static final Block BLOCK_MALACHITE = new BlockMetal("block_malachite", Material.IRON);
+	public static final Block BLOCK_YELLOW_DIAMOND = new BlockMetal("block_yellow_diamond", Material.IRON);
+	public static final Block BLOCK_PERIDOT = new BlockMetal("block_peridot", Material.IRON);	
+	
+	public static final Block BLOCK_COBALT = new BlockMetal("block_cobalt", Material.IRON);
+	public static final Block BLOCK_ADAMANTITE = new BlockMetal("block_adamantite", Material.IRON);
+	public static final Block BLOCK_MYTHRIL = new BlockMetal("block_mythril", Material.IRON);
+	public static final Block BLOCK_PALLADIUM = new BlockMetal("block_palladium", Material.IRON);
+	
+	public static final Block BLOCK_GALAXY = new BlockMetal("block_galaxy", Material.IRON);
+	
 	//overworld ores
 	public static final Block ORE_COPPER = new ToolWoodOre("ore_copper", Material.IRON);
 	public static final Block ORE_TIN = new ToolWoodOre("ore_tin", Material.IRON);
 	public static final Block ORE_NICKEL = new ToolWoodOre("ore_nickel", Material.IRON);
-	public static final Block ORE_COBALT = new OreCobalt("ore_cobalt", Material.IRON);
-	public static final Block ORE_AMETHYST = new OreAmethyst("ore_amethyst", Material.IRON);
+	public static final Block ORE_SILVER = new ToolWoodOre("ore_silver", Material.IRON);
+	public static final Block ORE_PLATINUM = new BlockMetal("ore_platinum", Material.IRON);
 	public static final Block ORE_RUBY = new OreRuby("ore_ruby", Material.IRON);
 	public static final Block ORE_SAPHIRE = new OreSaphire("ore_saphire", Material.IRON);
-	
-	//public static final Block ORE_MALACHITE = new ToolIronOre("ore_malachite", Material.IRON);
-	//public static final Block ORE_JADE = new ToolIronOre("ore_jade", Material.IRON);
-	//public static final Block ORE_PERIDOT = new ToolIronOre("ore_peridot", Material.IRON);
-	//public static final Block ORE_ROSE_QUARTZ = new ToolIronOre("ore_rose_quartz", Material.IRON);
-	//public static final Block ORE_TOPAZ = new ToolIronOre("ore_topaz", Material.IRON);
-	//public static final Block ORE_YELLOW_DIAMOND = new ToolIronOre("ore_yellow_diamond", Material.IRON);
+	public static final Block ORE_COBALT = new OreCobalt("ore_cobalt", Material.IRON);
+
 	
 	//nether ores
-	public static final Block ORE_ADAMANTITE = new OreAdamantite("ore_adamantite", Material.IRON);
 	public static final Block ORE_SULFUR_NETHER = new ToolWoodOre("ore_sulfur", Material.IRON);
 	public static final Block ORE_COAL_NETHER = new OreCoal("ore_coal_nether", Material.IRON);
 	public static final Block ORE_GOLD_NETHER = new ToolStoneOre("ore_gold_nether", Material.IRON);
 	public static final Block ORE_ONYX = new OreOnyx("ore_onyx", Material.IRON);
 	public static final Block ORE_GARNET = new OreGarnet("ore_garnet", Material.IRON);
+	public static final Block ORE_ROSE_QUARTZ = new ToolIronOre("ore_rose_quartz", Material.IRON);
+	public static final Block ORE_ADAMANTITE = new OreAdamantite("ore_adamantite", Material.IRON);
+	
+	//aether ores
+	public static final Block ORE_MALACHITE = new ToolIronOre("ore_malachite", Material.IRON);
+	public static final Block ORE_YELLOW_DIAMOND = new ToolIronOre("ore_yellow_diamond", Material.IRON);
+	public static final Block ORE_PERIDOT = new ToolIronOre("ore_peridot", Material.IRON);
+	public static final Block ORE_MYTHRIL = new OreAmethyst("ore_mythril", Material.IRON);
 	
 	//end ores
+	public static final Block ORE_AMETHYST = new OreAmethyst("ore_amethyst", Material.IRON);
+	public static final Block ORE_JADE = new OreAmethyst("ore_jade", Material.IRON);
+	public static final Block ORE_PHOSPHOLITE = new OreAmethyst("ore_phospholite", Material.IRON);
+	public static final Block ORE_PALLADIUM = new OreAmethyst("ore_palladium", Material.IRON);
 	
-	
-	//Blocks
-	public static final Block BLOCK_ADAMANTITE = new BlockMetal("block_adamantite", Material.IRON);
-	public static final Block BLOCK_COBALT = new BlockMetal("block_cobalt", Material.IRON);
-	public static final Block BLOCK_JADE = new BlockMetal("block_jade", Material.IRON);
-	public static final Block BLOCK_MALACHITE = new BlockMetal("block_malachite", Material.IRON);
-	public static final Block BLOCK_NICKEL = new BlockMetal("block_nickel", Material.IRON);
-	public static final Block BLOCK_ONYX = new BlockMetal("block_onyx", Material.IRON);
-	public static final Block BLOCK_PERIDOT = new BlockMetal("block_peridot", Material.IRON);
-	public static final Block BLOCK_ROSE_QUARTZ = new BlockMetal("block_rose_quartz", Material.IRON);
-	public static final Block BLOCK_RUBY = new BlockMetal("block_ruby", Material.IRON);
-	public static final Block BLOCK_SAPHIRE = new BlockMetal("block_saphire", Material.IRON);
-	public static final Block BLOCK_TOPAZ = new BlockMetal("block_topaz", Material.IRON);
-	public static final Block BLOCK_YELLOW_DIAMOND = new BlockMetal("block_yellow_diamond", Material.IRON);
+
 }
--- java/biofan/oresmod/init/ModRecipes.java
+++ java/biofan/oresmod/init/ModRecipes.java
@@ -16,5 +16,13 @@ public class ModRecipes
 		GameRegistry.addSmelting(ModBlocks.ORE_GOLD_NETHER, new ItemStack(Items.GOLD_INGOT, 1), 1.5f);
 		GameRegistry.addSmelting(ModItems.DUST_BRONZE, new ItemStack(ModItems.INGOT_BRONZE, 1), 1.5f);
 		GameRegistry.addSmelting(ModBlocks.ORE_COPPER, new ItemStack(ModItems.INGOT_COPPER, 1), 1.5f);
+		GameRegistry.addSmelting(ModBlocks.ORE_TIN, new ItemStack(ModItems.INGOT_TIN, 1), 1.5f);
+		GameRegistry.addSmelting(ModBlocks.ORE_NICKEL, new ItemStack(ModItems.INGOT_NICKEL, 1), 1.5f);
+		GameRegistry.addSmelting(ModBlocks.ORE_AMETHYST, new ItemStack(ModItems.GEM_AMETHYST, 1), 1.5f);
+		GameRegistry.addSmelting(ModBlocks.ORE_RUBY, new ItemStack(ModItems.GEM_RUBY, 1), 1.5f);
+		GameRegistry.addSmelting(ModBlocks.ORE_SAPHIRE, new ItemStack(ModItems.GEM_SAPHIRE, 1), 1.5f);
+		GameRegistry.addSmelting(ModBlocks.ORE_SULFUR_NETHER, new ItemStack(ModItems.SULFUR, 1), 1.5f);
+		GameRegistry.addSmelting(ModBlocks.ORE_ONYX, new ItemStack(ModItems.GEM_ONYX, 1), 1.5f);
+		GameRegistry.addSmelting(ModBlocks.ORE_GARNET, new ItemStack(ModItems.GEM_GARNET, 1), 1.5f);
 	}
 }
